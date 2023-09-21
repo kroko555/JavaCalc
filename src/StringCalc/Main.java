d4716fb2c92b9b7629634ab7a220451e4e2327a0
Kroko55 <89172197661@mail.ru>
2023-09-21 17:23:58 +0300

    Строковый калькулятор

--- src/StringCalc/Main.java
+++ src/StringCalc/Main.java
@@ -8,17 +8,14 @@ public class Main {
         String s1 = null;
         String s2 = null;
         String result = null;
-        String [] s3 = input.split("[-+*/]");
-        System.out.println(s3[0]);
-        System.out.println(s3[1]);
+        String input1 = input.replaceAll("\\s","");
+        String [] s3 = input1.split("[-+*/]");
         Pattern p = Pattern.compile("\"([^\"]*)\"");
         Matcher m = p.matcher(s3[0]);
         Matcher m1 = p.matcher(s3[1]);
-
         while (m.find()) {
             s1 = m.group(1);
         }
-        System.out.println(s1);
         if(s1.contains("1") ||s1.contains("2") ||s1.contains("3") ||s1.contains("4") ||s1.contains("5") ||
                 s1.contains("6") ||s1.contains("7") ||s1.contains("8") ||s1.contains("9") ||s1.contains("10")){
             throw new CalcException("Первое значение должно быть строкой");
@@ -49,10 +46,10 @@ public class Main {
                 int S2 = Integer.parseInt(s2);
                 StringD.ValueIntChecker(S2);
                 int endInd = s1.length() / S2;
-                result = s1.substring(0, endInd-1); StringD.ResultChecker(result); System.out.println(result); break;
+                result = s1.substring(0, endInd); StringD.ResultChecker(result); System.out.println(result); break;
             }
         }
 
-        return input;
+        return input1;
     }
 }
